85a4472b2d17747c4521f2cbae1423da9124e024
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-03-17 17:47:54 +0530

    working on add food item part

---
+++ src/app/lib/foodsModel.js
@@ -0,0 +1,13 @@
+const { default: mongoose } = require("mongoose");
+
+
+
+const foodModel = new mongoose({
+    name:String,
+    price:Number,
+    img_path:String,
+    description:String,
+    resto_id:mongoose.Schema.Types.ObjectId
+});
+
+export const foodSchema= mongoose.models.foods ||  mongoose.model("foods",foodModel);
