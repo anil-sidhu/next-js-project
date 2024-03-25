d72ae28bdb8b3fa429826b60e43774f26cc90452
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-03-25 17:08:19 +0530

    done

--- src/app/_components/FoodItemList.js
+++ src/app/_components/FoodItemList.js
@@ -1,5 +1,27 @@
-const FoodItemList=()=>{
-    return(<div>
+import { useEffect, useState } from "react";
+
+const FoodItemList = () => {
+    const [foodItems, setFoodItems] = useState();
+
+    useEffect(() => {
+        loadFoodItems();
+    }, []);
+
+    const loadFoodItems = async () => {
+        const restaurantData= JSON.parse(localStorage.getItem('restaurantUser'));
+        const resto_id= restaurantData._id;
+        let response = await fetch("http://localhost:3000/api/restaurant/foods/"+resto_id);
+        response = await response.json();
+        if (response.success) {
+            setFoodItems(response.result)
+        } else {
+            alert("food item list not loading")
+        }
+
+    }
+
+
+    return (<div>
         <h1>Food Items</h1>
         <table>
             <thead>
@@ -13,15 +35,20 @@ const FoodItemList=()=>{
                 </tr>
             </thead>
             <tbody>
-            <tr>
-                    <td>1</td>
-                    <td>Pizza</td>
-                    <td>300</td>
-                    <td>Best seller pizza</td>
-                    <td>Image</td>
-                    <td><button>Delete</button><button>Edit</button></td>
-                </tr>
-                
+                {
+                 foodItems &&  foodItems.map((item,key)=>(
+                        <tr key={key}>
+                        <td>{key+1}</td>
+                        <td>{item.name}</td>
+                        <td>{item.price}</td>
+                        <td>{item.description}</td>
+                        <td><img src={item.img_path} /> </td>
+                        <td><button>Delete</button><button>Edit</button></td>
+                    </tr>
+    
+                    ))
+                }
+               
             </tbody>
         </table>
 
--- src/app/api/restaurant/foods/[id]/route.js
+++ src/app/api/restaurant/foods/[id]/route.js
@@ -14,3 +14,4 @@ export async function GET(request,content){
     return NextResponse.json({result,success})
 
 }
+
