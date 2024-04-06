eb5e593bcb9971258c011efabb9d0795b10e3117
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-04-06 14:31:32 +0530

    api details

--- src/app/explore/[name]/page.js
+++ src/app/explore/[name]/page.js
@@ -1,12 +1,49 @@
+'use client'
 import CustomerHeader from "@/app/_components/CustomerHeader"
+import { useEffect, useState } from "react"
 
 const Page = (props) => {
-    const name=props.params.name
+    const name = props.params.name;
+    const [restaurantDetails, setRestaurantDetails] = useState();
+    const [foodItems, setFoodItems] = useState([])
+
+    useEffect(() => {
+        loadRestaurantDetails()
+    }, []);
+
+    const loadRestaurantDetails = async () => {
+        const id = props.searchParams.id;
+        let response = await fetch("http://localhost:3000/api/customer/" + id)
+        response = await response.json();
+        if (response.success) {
+            setRestaurantDetails(response.details)
+            setFoodItems(response.foodItems)
+        }
+
+    }
     return (
         <div>
             <CustomerHeader />
             <div className="restaurant-page-banner">
                 <h1>{decodeURI(name)}</h1>
+            </div>
+            <div>
+                <h3>{restaurantDetails?.contact}</h3>
+                <h3>{restaurantDetails?.city}</h3>
+                <h3>{restaurantDetails?.address}</h3>
+                <h3>{restaurantDetails?.email}</h3>
+            </div>
+            <div>
+                {
+                    foodItems.map((item) => (
+                        <div>
+                            <div>{item.name}</div>
+                            <div>{item.price}</div>
+                            <div>{item.description}</div>
+                            <img style={{width:100}} src={item.img_path} />
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -72,7 +72,7 @@ export default function Home() {
       <div className="restaurant-list-container">
         {
           restaurants.map((item) => (
-            <div onClick={()=>router.push('explore/'+item.name)} className="restaurant-wrapper">
+            <div onClick={()=>router.push('explore/'+item.name+"?id="+item._id)} className="restaurant-wrapper">
               <div className="heading-wrapper">
                 <h3>{item.name}</h3>
                 <h5>Contact:{item.contact}</h5>
