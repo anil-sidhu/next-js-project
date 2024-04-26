954490b6b91e861777e66c876c05f991d4bfdc17
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-04-26 19:05:39 +0530

    worked onm my order api

--- src/app/api/order/route.js
+++ src/app/api/order/route.js
@@ -1,17 +1,41 @@
 import { connectionStr } from "@/app/lib/db";
 import { orderSchema } from "@/app/lib/ordersMode";
+import { restaurantSchema } from "@/app/lib/restaurantsModel";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 
 export async function POST(request) {
     const payload = await request.json();
-    await mongoose.connect(connectionStr,{useNewUrlParser:true});
-    let success=false;
-    const orderObj= new orderSchema(payload);
+    await mongoose.connect(connectionStr, { useNewUrlParser: true });
+    let success = false;
+    const orderObj = new orderSchema(payload);
     const result = await orderObj.save();
-    if(result){
-        success=true
+    if (result) {
+        success = true
     }
-    return NextResponse.json({result,success})
+    return NextResponse.json({ result, success })
+}
+
+export async function GET(request) {
+    const userId = request.nextUrl.searchParams.get('id');
+    let success = false
+    await mongoose.connect(connectionStr, { useNewUrlParser: true })
+    let result = await orderSchema.find({ user_id: userId });
+    if (result) {
+        let restoData = await Promise.all(
+            result.map(async (item) => {
+                let restoInfo = {};
+                restoInfo.data = await restaurantSchema.findOne({ _id: item.resto_id })
+                restoInfo.amount = item.amount;
+                restoInfo.status = item.status;
+                return restoInfo;
+            })
+        )
+        result = restoData;
+        success = true
+    }
+
+    return NextResponse.json({ result })
+
 }
--- src/app/order/page.js
+++ src/app/order/page.js
@@ -9,94 +9,93 @@ import { useRouter } from "next/navigation"
 
 const Page = () => {
 
-    const [userStorage,setUserStorage]=useState(JSON.parse(localStorage.getItem('user')));
+    const [userStorage, setUserStorage] = useState(JSON.parse(localStorage.getItem('user')));
     const [cartStorage, setCartStorage] = useState(JSON.parse(localStorage.getItem('cart')))
-    const [total]=useState(()=>cartStorage?.length==1?cartStorage[0].price:cartStorage?.reduce((a,b)=>{
-return a.price+b.price
+    const [total] = useState(() => cartStorage?.length == 1 ? cartStorage[0].price : cartStorage?.reduce((a, b) => {
+        return a.price + b.price
     }))
-    console.log(total);
 
-    const [removeCartData,setRemoveCartData]=useState(false)
+    const [removeCartData, setRemoveCartData] = useState(false)
     const router = useRouter()
 
 
-    useEffect(()=>{
-if(!total){
-    router.push('/')
-}
-    },[total])
+    useEffect(() => {
+        if (!total) {
+            router.push('/')
+        }
+    }, [total])
 
-    const orderNow=async()=>{
-        let user_id=JSON.parse(localStorage.getItem('user'))._id;
+    const orderNow = async () => {
+        let user_id = JSON.parse(localStorage.getItem('user'))._id;
         let cart = JSON.parse(localStorage.getItem('cart'));
-        let foodItemIds=cart.map((item)=>item._id).toString();
-        let deliveryBoy_id= '660c42156c1b5373566d55bc';
-        
+        let foodItemIds = cart.map((item) => item._id).toString();
+        let deliveryBoy_id = '660c42156c1b5373566d55bc';
 
-        let resto_id=cart[0].resto_id;
-        let collection ={
+
+        let resto_id = cart[0].resto_id;
+        let collection = {
             user_id,
             resto_id,
             foodItemIds,
             deliveryBoy_id,
-            status:'confirm',
-            amount:total+DELIVERY_CHARGES+(total*TAX/100),
+            status: 'confirm',
+            amount: total + DELIVERY_CHARGES + (total * TAX / 100),
         }
 
-        let response = await fetch('http://localhost:3000/api/order',{
-            method:'POST',
-            body:JSON.stringify(collection)
+        let response = await fetch('http://localhost:3000/api/order', {
+            method: 'POST',
+            body: JSON.stringify(collection)
         });
         response = await response.json();
-        if(response.success){
+        if (response.success) {
             alert("order confirmed")
             setRemoveCartData(true)
             router.push('myprofile')
 
-        }else{
+        } else {
             alert("order failed")
         }
         console.log(collection);
     }
     return (
         <div>
-            <CustomerHeader  removeCartData={removeCartData} />
+            <CustomerHeader removeCartData={removeCartData} />
             <div className="total-wrapper">
-               <div className="block-1">
-                <h2>User Details</h2>
-               <div className="row">
-                    <span>Name </span>
-                    <span>{userStorage.name}</span>
+                <div className="block-1">
+                    <h2>User Details</h2>
+                    <div className="row">
+                        <span>Name </span>
+                        <span>{userStorage.name}</span>
+                    </div>
+                    <div className="row">
+                        <span>address </span>
+                        <span>{userStorage.address}</span>
+                    </div>
+                    <div className="row">
+                        <span>Mobile </span>
+                        <span>{userStorage.mobile}</span>
+                    </div>
+                    <h2>Amount Details</h2>
+                    <div className="row">
+                        <span>Tax : </span>
+                        <span>{total * TAX / 100}</span>
+                    </div>
+                    <div className="row">
+                        <span>Delivery Charges  : </span>
+                        <span>{DELIVERY_CHARGES}</span>
+                    </div>
+                    <div className="row">
+                        <span>Total Amount : </span>
+                        <span>{total + DELIVERY_CHARGES + (total * TAX / 100)}</span>
+                    </div>
+                    <h2>Payment Methods</h2>
+                    <div className="row">
+                        <span>Cash on Delivery : </span>
+                        <span>{total + DELIVERY_CHARGES + (total * TAX / 100)}</span>
+                    </div>
+
                 </div>
-                <div className="row">
-                    <span>address </span>
-                    <span>{userStorage.address}</span>
-                </div>
-                <div className="row">
-                    <span>Mobile </span>
-                    <span>{userStorage.mobile}</span>
-                </div>
-                <h2>Amount Details</h2>
-                <div className="row">
-                    <span>Tax : </span>
-                    <span>{total*TAX/100}</span>
-                </div>
-                <div className="row">
-                    <span>Delivery Charges  : </span>
-                    <span>{DELIVERY_CHARGES}</span>
-                </div>
-                <div className="row">
-                    <span>Total Amount : </span>
-                    <span>{total+DELIVERY_CHARGES+(total*TAX/100)}</span>
-                </div>
-                <h2>Payment Methods</h2>
-                <div className="row">
-                    <span>Cash on Delivery : </span>
-                    <span>{total+DELIVERY_CHARGES+(total*TAX/100)}</span>
-                </div>
-                
-               </div>
-               <div className="block-2">
+                <div className="block-2">
                     <button onClick={orderNow} >Place your Order Now</button>
                 </div>
             </div>
